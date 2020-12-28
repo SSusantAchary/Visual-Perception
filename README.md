ab18061bfecd20bc03c4518d5e0903c25a8e9a71
Susant Achary <acharysusant@gmail.com>
2020-12-29 00:44:36 +0530

    visual perception lib

---
+++ README.md
@@ -0,0 +1,50 @@
+# Visual-Perception
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](LICENSE.txt)
+
+Visual Perception is a High Level Python Library to performs various task under visual recognition.
+
+Currently, It Provides the Solution for Object Detection.
+
+For Detection with pre-trained models it provides:
+  - YOLOv4
+  - TinyYOLOv4
+
+
+### Dependencies:
+  - tensorflow >= 2.3.0
+  - keras
+  - opencv-python
+  - numpy
+  - pillow
+  - matplotlib
+  - pandas
+  - scikit-learn
+  - scikit-image
+  - imgaug
+  - labelme2coco
+  - progressbar2
+  - scipy
+  - h5py
+  - configobj
+
+
+## **`Get Started:`**
+```python
+!pip install visual_perception
+```
+
+
+### **`Image Object Detection` Using `YOLOv4`** 
+
+```python
+from visual_perception.Detection import Object_Detection
+
+model = Object_Detection()
+model.Det_YOLOv4()
+model.Detect_From_Image(input_path='car.jpg',
+                        output_path='./output_image.jpg')
+
+from PIL import Image
+Image.open('output_image.jpg')
+```
---
+++ setup.py
@@ -0,0 +1,64 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='visual_perception',
+    description='A High Level Python Library for Visual Recognition ',
+    url="https://github.com/SSusantAchary/Visual-Perception",
+    author="SusantAchary",
+    author_email="sache.meet@yahoo.com",
+    maintainer="Susant Achary",
+    maintainer_email="sache.meet@yahoo.com",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    python_requires='>=3.5, <4',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Research',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Scientific/Engineering :: Image Processing',
+        'Topic :: Scientific/Engineering :: Image Recognition',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    keywords=[
+        "object detection",
+        "computer vision",
+        'yolo',
+        'yolov4',
+        'tinyyolo'],
+
+    install_requires=[
+        "tensorflow",
+        'keras',
+        'opencv-python',
+        'numpy',
+        'pillow',
+        'matplotlib',
+        'pandas',
+        'scikit-learn',
+        'progressbar2',
+        'scipy',
+        'h5py',
+        'imgaug',
+        'scikit-image',
+        'labelme2coco',
+        'configobj',
+    ],
+
+    project_urls={
+
+        'Bug Reports': 'https://github.com/SSusantAchary/Visual-Perception/issues',
+        'Source': 'https://github.com/SSusantAchary/Visual-Perception/'},
+
+)
